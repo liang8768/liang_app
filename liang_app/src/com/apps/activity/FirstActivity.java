166bdb7e0ffcff8e59b890d37f3b6927191a49bc
Liang <liangliang8768@qq.com>
2014-06-18 17:34:11 +0800

    测试

---
+++ liang_app/src/com/apps/activity/FirstActivity.java
@@ -0,0 +1,21 @@
+package com.apps.activity;
+
+import com.apps.liang_app.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * appÊ×Ò³
+ * 
+ * @author Liang
+ *
+ */
+public class FirstActivity extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+	
+}
